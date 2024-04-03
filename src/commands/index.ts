1e4491167d7a1ea05c8464669335a834b7c1958a
fwwz-id <fwwz.id@proton.me>
2024-04-03 11:55:04 +0700

    feat(commands): add profile and join commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,9 +1,13 @@
 import { default as ping } from "./ping";
 import { default as hero } from "./hero";
+import { default as profile } from "./profile";
+import { default as join } from "./join";
 
 const commands = {
   ping,
   hero,
+  profile,
+  join,
 };
 
 export type ExportCommandsKey = keyof typeof commands;
