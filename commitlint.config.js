4fb4dab26ba63831d284b43dbdddaa61423ce299
fwwz-id <fwwz.id@proton.me>
2024-04-03 11:36:52 +0700

    chore: update commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,6 +4,7 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
+    "body-max-line-length": [1, "always", Infinity],
     "footer-max-line-length": [1, "always", Infinity],
   },
 };
